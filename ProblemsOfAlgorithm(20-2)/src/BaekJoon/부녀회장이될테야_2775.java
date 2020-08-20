86654ec5eaa21be46e31f2fde20f8b81d60937e2
kimminkwon <kmk130519@gmail.com>
2020-08-20 14:57:50 +0900

    Problem Solving (Baekjoon 2775)
    200820

---
+++ ProblemsOfAlgorithm(20-2)/src/BaekJoon/부녀회장이될테야_2775.java
@@ -0,0 +1,63 @@
+package BaekJoon;
+
+import java.util.*;
+import java.io.*;
+
+public class 부녀회장이될테야_2775 {
+	static int n;
+	static int[] floor;
+	static int[] houseNumber;
+	static long[][] numOfPeoples;
+	static long[] results;
+	
+	public static void main(String[] args) {
+		makeInput();
+		preprocessingNumOfPeoples();
+		findNumOfPeoples();
+		printResult();
+	}
+	
+	private static void printResult() {
+		for(int i = 0; i < n; i++) {
+			System.out.println(results[i]);
+		}
+	}
+
+	private static void findNumOfPeoples() {
+		for(int i = 0; i < n; i++) {
+			results[i] = findNumOfPeople(floor[i], houseNumber[i]);
+		}
+	}
+
+	private static long findNumOfPeople(int floor, int houseNumber) {
+		return numOfPeoples[floor][houseNumber];
+	}
+
+	private static void makeInput() {
+		Scanner input = new Scanner(System.in);
+		n = input.nextInt();
+		
+		floor = new int[n];
+		houseNumber = new int[n];
+		numOfPeoples = new long[15][15];
+		results = new long[n];
+		
+		for(int i = 0; i < n; i++) {
+			floor[i] = input.nextInt();
+			houseNumber[i] = input.nextInt();
+		}
+	}
+	
+	private static void preprocessingNumOfPeoples() {
+		for(int i = 1; i <= 14; i++) { // 0층 저장
+			numOfPeoples[0][i] = i;
+		}
+		
+		for(int i = 1; i < 15; i++) { // 1층부터 14층까지
+			for(int j = 1; j < 15; j++) {
+				// i층 j호
+				numOfPeoples[i][j] = numOfPeoples[i][j-1] + numOfPeoples[i-1][j];
+			}
+		}
+	}
+}
