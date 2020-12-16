43cfbfd320d8e435c59a5ee4df454fb6835ddd7a
kimminkwon <kmk130519@gmail.com>
2020-12-16 12:58:38 +0900

    Problem Solving (Programmers 타겟넘버)
    201216

---
+++ ProblemsOfAlgorithm(20-2)/src/Programmers/Level2_타겟넘버.java
@@ -0,0 +1,41 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class Level2_타겟넘버 {
+
+    private static int result;
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 3;
+        boolean[] isSelected = new boolean[numbers.length];
+        result = 0;
+        makeTargetValue(0, 0, isSelected, numbers, target);
+
+        System.out.println(result);
+    }
+
+    private static void makeTargetValue(int value, int index, boolean[] isSelected, int[] numbers, int target) {
+        if(index == numbers.length) {
+            if(value == target) {
+                result++;
+            }
+            return;
+        }
+
+        isSelected[index] = true;
+
+        makeTargetValue(value + numbers[index], index + 1, isSelected.clone(), numbers, target);
+        makeTargetValue(value - numbers[index], index + 1, isSelected.clone(), numbers, target);
+
+        isSelected[index] = false;
+    }
+
+    private static boolean isTrue(boolean[] isSelected) {
+        for(boolean b : isSelected) {
+            if(b == false) return false;
+        }
+        return true;
+    }
+}
